69ac850eb3842f822b2f9a508dbb4036771c951b
Daniel Steinberg <daniel@dimsumthinking.com>
2014-01-03 12:12:08 -0500

    Implemented methods for Spaceship

---
+++ 02ObjectiveCSyntax/05Spaceship/Spaceship/SCSSpaceship.m
@@ -0,0 +1,23 @@
+#import "SCSSpaceship.h"
+
+@implementation SCSSpaceship {
+    double _topSpeed;
+    SCSPlanet *_planetToOrbit;
+    double _altitude;
+}
+- (SCSPlanet *)planetToOrbit {
+    return _planetToOrbit;
+}
+- (double)topSpeed {
+    return _topSpeed;
+}
+- (void)setTopSpeed:(double)topSpeed {
+    _topSpeed = topSpeed;
+}
+- (void)orbitPlanet:(SCSPlanet *)planet
+         atAltitude:(double)altitude {
+    _planetToOrbit = planet;
+    _altitude = altitude;
+}
+- (void)engage {}
+@end
---
+++ 02ObjectiveCSyntax/05Spaceship/SpaceshipTests/SpaceshipTests.m
@@ -0,0 +1,34 @@
+//
+//  SpaceshipTests.m
+//  SpaceshipTests
+//
+//  Created by Daniel Steinberg on 1/2/14.
+//  Copyright (c) 2014 CS 193P. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SpaceshipTests : XCTestCase
+
+@end
+
+@implementation SpaceshipTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
