4864babd43a0594b2fc87aaf0f2f1290ed013d4b
Daniel Steinberg <daniel@dimsumthinking.com>
2014-01-11 15:56:06 -0500

    Made a copy so I can modify colors

---
+++ 04Xcode/05HitMe/HitMe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HitMe
+//
+//  Created by Daniel Steinberg on 1/10/14.
+//  Copyright (c) 2014 CS 193P. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SCSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SCSAppDelegate class]));
+    }
+}
