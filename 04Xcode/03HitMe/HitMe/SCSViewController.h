4285737c3043098fed2465848d119d1c1b891f72
Daniel Steinberg <daniel@dimsumthinking.com>
2014-01-11 10:44:11 -0500

    Copied HitMe for the IB phase

---
+++ 04Xcode/03HitMe/HitMe/SCSViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCSViewController.h
+//  HitMe
+//
+//  Created by Daniel Steinberg on 1/10/14.
+//  Copyright (c) 2014 CS 193P. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCSViewController : UIViewController
+
+@end
---
+++ 04Xcode/03HitMe/HitMeTests/HitMeTests.m
@@ -0,0 +1,34 @@
+//
+//  HitMeTests.m
+//  HitMeTests
+//
+//  Created by Daniel Steinberg on 1/10/14.
+//  Copyright (c) 2014 CS 193P. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HitMeTests : XCTestCase
+
+@end
+
+@implementation HitMeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
