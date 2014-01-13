deb852ed6bb8cae6c32846fe3a82a46e7457fe8a
Daniel Steinberg <daniel@dimsumthinking.com>
2014-01-13 08:44:04 -0500

    Finished actions

--- 04Xcode/07HitMe/HitMe/SCSViewController.m
+++ 04Xcode/07HitMe/HitMe/SCSViewController.m
@@ -8,14 +8,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.playingCardLabel.layer.cornerRadius = self.playingCardLabel.frame.size.width/12;
+    self.playingCardLabel.layer.cornerRadius
+        = self.playingCardLabel.frame.size.width/12;
     self.playingCardLabel.text = @"A ♣︎";
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)flipCardButtonTapped:(UIButton *)sender {
+    if ([self.playingCardLabel.textColor isEqual:[UIColor whiteColor]]) {
+        self.playingCardLabel.textColor = [UIColor blackColor];
+    } else self.playingCardLabel.textColor = [UIColor whiteColor];
 }
+
+- (IBAction)nextCardButtonTapped:(UIButton *)sender {
+    if ([self.playingCardLabel.text isEqualToString:@"A ♣︎"]) {
+        self.playingCardLabel.text = @"3 ♠︎";
+    } else self.playingCardLabel.text = @"A ♣︎";
+}
+
+
+
 
 @end
