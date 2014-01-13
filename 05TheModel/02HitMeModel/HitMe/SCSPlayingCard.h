7f988977fa46ed2ae276f7167d37707bd2b2001d
Daniel Steinberg <daniel@dimsumthinking.com>
2014-01-13 16:48:49 -0500

    Made copy for group section.

---
+++ 05TheModel/02HitMeModel/HitMe/SCSPlayingCard.h
@@ -0,0 +1,13 @@
+//
+//  SCSPlayingCard.h
+//  HitMe
+//
+//  Created by Daniel Steinberg on 1/13/14.
+//  Copyright (c) 2014 CS 193P. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SCSPlayingCard : NSObject
+
+@end
---
+++ 05TheModel/02HitMeModel/HitMe/SCSPlayingCard.m
@@ -0,0 +1,13 @@
+//
+//  SCSPlayingCard.m
+//  HitMe
+//
+//  Created by Daniel Steinberg on 1/13/14.
+//  Copyright (c) 2014 CS 193P. All rights reserved.
+//
+
+#import "SCSPlayingCard.h"
+
+@implementation SCSPlayingCard
+
+@end
