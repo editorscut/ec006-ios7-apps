53fe3c62d9cc9175c60f8cb01ef2531277635bc8
Daniel Steinberg <daniel@dimsumthinking.com>
2014-01-13 17:08:01 -0500

    Created Playing card again

--- 05TheModel/02HitMeModel/HitMe/SCSPlayingCard.h
+++ 05TheModel/02HitMeModel/HitMe/SCSPlayingCard.h
@@ -1,11 +1,3 @@
-//
-//  SCSPlayingCard.h
-//  HitMe
-//
-//  Created by Daniel Steinberg on 1/13/14.
-//  Copyright (c) 2014 CS 193P. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 @interface SCSPlayingCard : NSObject
--- 05TheModel/02HitMeModel/HitMe/SCSPlayingCard.m
+++ 05TheModel/02HitMeModel/HitMe/SCSPlayingCard.m
@@ -1,11 +1,3 @@
-//
-//  SCSPlayingCard.m
-//  HitMe
-//
-//  Created by Daniel Steinberg on 1/13/14.
-//  Copyright (c) 2014 CS 193P. All rights reserved.
-//
-
 #import "SCSPlayingCard.h"
 
 @implementation SCSPlayingCard
