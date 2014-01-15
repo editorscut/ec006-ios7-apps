1a1958cb94d463ba1fc7bc5f44e71457c1e55992
Daniel Steinberg <daniel@dimsumthinking.com>
2014-01-15 17:18:05 -0500

    Added test class

--- 05TheModel/04HitMeModel/HitMeTests/SCSPlayingCardTests.m
+++ 05TheModel/04HitMeModel/HitMeTests/SCSPlayingCardTests.m
@@ -1,11 +1,3 @@
-//
-//  SCSPlayingCardTests.m
-//  HitMe
-//
-//  Created by Daniel Steinberg on 1/15/14.
-//  Copyright (c) 2014 CS 193P. All rights reserved.
-//
-
 #import <XCTest/XCTest.h>
 
 @interface SCSPlayingCardTests : XCTestCase
@@ -14,20 +6,15 @@
 
 @implementation SCSPlayingCardTests
 
-- (void)setUp
-{
+- (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+- (void)tearDown {
     [super tearDown];
 }
 
-- (void)testExample
-{
+- (void)testExample {
     XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
 }
 
