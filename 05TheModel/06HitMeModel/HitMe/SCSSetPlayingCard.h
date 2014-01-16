c2aa16e4bcd051dd0c6d93360dbd512c95e160cf
Daniel Steinberg <daniel@dimsumthinking.com>
2014-01-16 09:03:14 -0500

    Demo of instancetype with a failing test.

---
+++ 05TheModel/06HitMeModel/HitMe/SCSSetPlayingCard.h
@@ -0,0 +1,5 @@
+#import "SCSPlayingCard.h"
+
+@interface SCSSetPlayingCard : SCSPlayingCard
+@property (nonatomic) NSString *fillStyle;
+@end
---
+++ 05TheModel/06HitMeModel/HitMe/SCSSetPlayingCard.m
@@ -0,0 +1,5 @@
+#import "SCSSetPlayingCard.h"
+
+@implementation SCSSetPlayingCard
+
+@end
