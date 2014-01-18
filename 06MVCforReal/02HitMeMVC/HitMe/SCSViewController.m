b16b3fdce9db6799647a144d310f73c38295c336
Daniel Steinberg <daniel@dimsumthinking.com>
2014-01-18 15:07:15 -0500

    Added view and controller classes for playing card

---
+++ 06MVCforReal/02HitMeMVC/HitMe/SCSViewController.m
@@ -0,0 +1,47 @@
+#import "SCSViewController.h"
+#import "SCSHitMeGame.h"
+#import "SCSPlayingCard.h"
+
+@interface SCSViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *playingCardLabel;
+@property (nonatomic) SCSHitMeGame *game;
+@property (nonatomic) BOOL isFaceDown;
+@property (nonatomic) SCSPlayingCard *currentPlayingCard;
+@end
+
+@implementation SCSViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.playingCardLabel.layer.cornerRadius = self.playingCardLabel.frame.size.width/12;
+    self.game = [[SCSHitMeGame alloc] init];
+    [self.game fillAndShuffleDeck];
+    [self getNextCard];
+}
+
+- (void)redrawCard {
+    self.playingCardLabel.text
+         = [NSString stringWithFormat:@"%@ %@", self.currentPlayingCard.rank,
+                                                self.currentPlayingCard.suit];
+    if ( self.isFaceDown) {
+        self.playingCardLabel.textColor = [UIColor whiteColor];
+    } else {
+        self.playingCardLabel.textColor = self.currentPlayingCard.color;
+    }
+}
+
+- (IBAction)flipCardButtonTapped:(UIButton *)sender {
+    self.isFaceDown = ! self.isFaceDown;
+    [self redrawCard];
+}
+
+- (void)getNextCard {
+    self.currentPlayingCard = [self.game nextCard];
+    [self redrawCard];
+}
+
+- (IBAction)nextCardButtonTapped:(UIButton *)nextCardButton {
+    [self getNextCard];
+    nextCardButton.enabled = [self.game hasNextCard];
+}
+@end
---
+++ 06MVCforReal/02HitMeMVC/HitMeTests/SCSHitMeGameTests.m
@@ -0,0 +1,57 @@
+#import <XCTest/XCTest.h>
+#import "SCSHitMeGame.h"
+#import "SCSPlayingCard.h"
+
+@interface SCSHitMeGameTests : XCTestCase
+@property (nonatomic) SCSHitMeGame *game;
+@end
+
+@interface SCSHitMeGame (Testing)
+- (void)fillDeck;
+@end
+
+@implementation SCSHitMeGameTests
+
+- (void)setUp {
+    [super setUp];
+    self.game = [[SCSHitMeGame alloc] init];
+    [self.game fillDeck];
+}
+
+- (void)tearDown {
+    [super tearDown];
+    self.game = nil;
+}
+- (void)testGameExists {
+    XCTAssertNotNil(self.game, @"The game should exist.");
+}
+
+- (void)testGameType {
+    XCTAssertEqualObjects([self.game class], [SCSHitMeGame class], @"Class should be SCSHitMeGame.");
+}
+
+- (void)testNewGameDeck {
+    SCSPlayingCard *firstCard = [self.game nextCard];
+    XCTAssertNotNil(firstCard, @"There should be a first card");
+    XCTAssertEqualObjects(firstCard.rank, @"A", @"The first card is an ace");
+    XCTAssertEqualObjects(firstCard.suit, @"♣︎", @"The first card is a club");
+    for (int i = 1; i < 51; i++) {
+        [self.game nextCard];
+    }
+    SCSPlayingCard *lastCard = [self.game nextCard];
+    XCTAssertNotNil(lastCard, @"There should be a first card");
+    XCTAssertEqualObjects(lastCard.rank, @"K", @"The first card is an ace");
+    XCTAssertEqualObjects(lastCard.suit, @"♦︎", @"The first card is a club");
+}
+- (void)testHasNextCard {
+    [self.game nextCard];
+    XCTAssertTrue([self.game hasNextCard], @"There should be a next card.");
+    for (int i = 1; i < 52; i++) {
+        [self.game nextCard];
+    }
+    XCTAssertFalse([self.game hasNextCard], @"There should not be a next card.");
+
+}
+
+
+@end
