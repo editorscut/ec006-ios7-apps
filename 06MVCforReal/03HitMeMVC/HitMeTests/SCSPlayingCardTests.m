fa5fd3425539fe9dff62f7d95f20dbcd20fde9e4
Daniel Steinberg <daniel@dimsumthinking.com>
2014-01-18 21:33:04 -0500

    Added label to container view

---
+++ 06MVCforReal/03HitMeMVC/HitMeTests/SCSPlayingCardTests.m
@@ -0,0 +1,35 @@
+#import <XCTest/XCTest.h>
+#import "SCSPlayingCard.h"
+
+@interface SCSPlayingCardTests : XCTestCase
+@property (nonatomic) SCSPlayingCard *playingCard;
+@end
+
+@implementation SCSPlayingCardTests
+
+- (void)setUp {
+    [super setUp];
+    self.playingCard = [[SCSPlayingCard alloc] initWithRank:@"A"
+                                                       suit:@"♣︎"
+                                                      color:[UIColor blackColor]];
+}
+
+- (void)tearDown {
+    [super tearDown];
+    self.playingCard = nil;
+}
+- (void)testPlayingCardExists {
+    XCTAssertNotNil(self.playingCard, @"The playing card should exist.");
+}
+
+- (void)testPlayingCardType {
+    XCTAssertEqualObjects([self.playingCard class], [SCSPlayingCard class], @"Class should be SCSPlayingCard");
+}
+
+- (void)testCardIsAceOfClubs {
+    XCTAssertEqualObjects(self.playingCard.rank, @"A", @"Rank should be A");
+    XCTAssertEqualObjects(self.playingCard.suit, @"♣︎", @"Suit should be ♣︎");
+    XCTAssertEqualObjects(self.playingCard.color, [UIColor blackColor], @"Color should be black");
+}
+
+@end
