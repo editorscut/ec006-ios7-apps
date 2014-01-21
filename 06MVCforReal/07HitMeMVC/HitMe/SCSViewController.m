5b0e95246c0aac216495d151ddd0a0e8f72ab6ca
Daniel Steinberg <daniel@dimsumthinking.com>
2014-01-21 08:54:47 -0500

    Added copy of project for refactoring section

---
+++ 06MVCforReal/07HitMeMVC/HitMe/SCSViewController.m
@@ -0,0 +1,41 @@
+#import "SCSViewController.h"
+#import "SCSHitMeGame.h"
+#import "SCSPlayingCard.h"
+
+@interface SCSViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *playingCardLabel;
+@property (nonatomic) SCSHitMeGame *game;
+@property (nonatomic) BOOL isFaceDown;
+@property (nonatomic) SCSPlayingCard *currentPlayingCard;
+@end
+
+@implementation SCSViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.game = [[SCSHitMeGame alloc] init];
+    [self.game fillAndShuffleDeck];
+    [self getNextCard];
+}
+
+- (void)redrawCard {
+    self.playingCardLabel.text
+         = [NSString stringWithFormat:@"%@ %@", self.currentPlayingCard.rank,
+                                                self.currentPlayingCard.suit];
+    if ( self.isFaceDown) {
+        self.playingCardLabel.textColor = [UIColor whiteColor];
+    } else {
+        self.playingCardLabel.textColor = self.currentPlayingCard.color;
+    }
+}
+
+- (void)getNextCard {
+    self.currentPlayingCard = [self.game nextCard];
+    [self redrawCard];
+}
+
+- (IBAction)nextCardButtonTapped:(UIButton *)nextCardButton {
+    [self getNextCard];
+    nextCardButton.enabled = [self.game hasNextCard];
+}
+@end
