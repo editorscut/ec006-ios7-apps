663342c391d2e2d5b55893241790d64b67c5304f
Daniel Steinberg <daniel@dimsumthinking.com>
2014-02-04 12:41:57 -0500

    Started chapter 7

---
+++ 07CustomViews/MatchMe/MatchMe/SCSViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCSViewController.h
+//  MatchMe
+//
+//  Created by Daniel Steinberg on 2/4/14.
+//  Copyright (c) 2014 CS 193P. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCSViewController : UIViewController
+
+@end
