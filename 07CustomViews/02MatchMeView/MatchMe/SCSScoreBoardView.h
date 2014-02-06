14e9023146a3f22062a2664e0b0d060eb60cc009
Daniel Steinberg <daniel@dimsumthinking.com>
2014-02-06 10:49:16 -0500

    Readded stage 2 of match game

---
+++ 07CustomViews/02MatchMeView/MatchMe/SCSScoreBoardView.h
@@ -0,0 +1,13 @@
+//
+//  SCSScoreBoardView.h
+//  MatchMe
+//
+//  Created by Daniel Steinberg on 2/4/14.
+//  Copyright (c) 2014 CS 193P. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCSScoreBoardView : UICollectionReusableView
+
+@end
