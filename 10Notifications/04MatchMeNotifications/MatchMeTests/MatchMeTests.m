38431300b4d14446d4cb82d65c1b40bdd94233de
Daniel Steinberg <daniel@dimsumthinking.com>
2014-02-26 15:38:01 -0500

    Added code to post notifications

---
+++ 10Notifications/04MatchMeNotifications/MatchMeTests/MatchMeTests.m
@@ -0,0 +1,34 @@
+//
+//  MatchMeTests.m
+//  MatchMeTests
+//
+//  Created by Daniel Steinberg on 2/4/14.
+//  Copyright (c) 2014 CS 193P. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MatchMeTests : XCTestCase
+
+@end
+
+@implementation MatchMeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
