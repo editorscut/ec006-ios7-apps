e95775cf9fd144c336109d0a10353ac033f8ff41
Daniel Steinberg <daniel@dimsumthinking.com>
2014-03-15 10:55:33 -0400

    Totally changed my mind about where to start this chapter.

--- 12SeguesAndRelationships/01MultipleScenes/MultipleScenes/SCSViewController.m
+++ 12SeguesAndRelationships/01MultipleScenes/MultipleScenes/SCSViewController.m
@@ -1,12 +1,7 @@
 #import "SCSViewController.h"
 
 @interface SCSViewController ()
-@property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
-
 @end
 
 @implementation SCSViewController
-
-
-
 @end
