7c9b245ed2328c3fc11c50d817a121570d5c7be6
Daniel Steinberg <daniel@dimsumthinking.com>
2014-03-15 10:23:20 -0400

    Creating new MultipleScenes project

---
+++ 12SeguesAndRelationships/01MultipleScenes/MultipleScenes/SCSViewController.m
@@ -0,0 +1,12 @@
+#import "SCSViewController.h"
+
+@interface SCSViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
+
+@end
+
+@implementation SCSViewController
+
+
+
+@end
