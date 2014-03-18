024644b838f6b2fd3ab948542d3ae588ea56560e
Daniel Steinberg <daniel@dimsumthinking.com>
2014-03-18 08:13:11 -0400

    Added new two scene sequence

--- 12SeguesAndRelationships/06MultipleScenes/MultipleScenes/SCSNameViewController.m
+++ 12SeguesAndRelationships/06MultipleScenes/MultipleScenes/SCSNameViewController.m
@@ -1,7 +1,7 @@
 #import "SCSNameViewController.h"
 
 @interface SCSNameViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
 @end
 
 @implementation SCSNameViewController
