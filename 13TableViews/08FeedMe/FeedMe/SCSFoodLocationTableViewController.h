d4c0df4d8e72dd2d3925d2fc1bb530ec91bb35f3
Daniel Steinberg <daniel@dimsumthinking.com>
2014-04-02 09:07:19 -0400

    Fixed Scales to fit

--- 13TableViews/08FeedMe/FeedMe/SCSFoodLocationTableViewController.h
+++ 13TableViews/08FeedMe/FeedMe/SCSFoodLocationTableViewController.h
@@ -1,11 +1,3 @@
-//
-//  SCSFoodLocationsTableViewController.h
-//  FeedMe
-//
-//  Created by Daniel Steinberg on 3/25/14.
-//  Copyright (c) 2014 CS 193P. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface SCSFoodLocationTableViewController : UITableViewController
